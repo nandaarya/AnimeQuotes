584da8acea4e82beb217985a94325de3463328bd
Nanda Arya Putra <nandaarya404@gmail.com>
2024-03-26 17:28:57 +0700

    fix layout

--- app/src/main/java/com/example/animequotes/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/animequotes/ui/detail/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.example.animequotes.R
 import com.example.animequotes.databinding.FragmentDetailBinding
 import com.example.core.domain.model.Quote
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -35,6 +36,33 @@ class DetailFragment : Fragment() {
     }
 
     private fun setContent(quote: Quote?) {
-        TODO("Not yet implemented")
+        quote?.let {
+            val quoteText = context?.getString(com.example.core.R.string.quote, it.quote)
+            binding?.tvQuote?.text = quoteText
+            binding?.tvCharacterName?.text = it.character
+            binding?.tvAnimeName?.text = it.anime
+
+            // Logika untuk mengatur gambar favorit
+            if (it.isFavorite) {
+                binding?.btnFavorite?.apply {
+                    setImageResource(R.drawable.ic_favorite_filled_24)
+                }
+            } else {
+                binding?.btnFavorite?.apply {
+                    setImageResource(R.drawable.ic_favorite_border_24)
+                }
+            }
+
+            // Aksi klik pada tombol favorit
+            binding?.btnFavorite?.setOnClickListener {
+                // Implementasikan logika untuk mengubah status favorit
+                // Anda dapat memanggil fungsi pada ViewModel untuk melakukan ini
+            }
+
+            // Aksi klik pada tombol bagikan
+            binding?.btnShare?.setOnClickListener {
+                // Implementasikan logika untuk berbagi kutipan
+            }
+        }
     }
 }
