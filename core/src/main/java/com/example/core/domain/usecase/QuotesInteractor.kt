594b7b7d49b0cf2f1bf39d99a535a09681a6bcb3
Nanda Arya Putra <nandaarya404@gmail.com>
2024-03-28 21:14:47 +0700

    fix for dependency rule

--- core/src/main/java/com/example/core/domain/usecase/QuotesInteractor.kt
+++ core/src/main/java/com/example/core/domain/usecase/QuotesInteractor.kt
@@ -1,12 +1,10 @@
 package com.example.core.domain.usecase
 
-import com.example.core.data.remote.network.ApiResponse
 import com.example.core.domain.model.Quote
 import com.example.core.domain.repository.IQuotesRepository
-import kotlinx.coroutines.flow.Flow
 
 class QuotesInteractor(private val quotesRepository: IQuotesRepository): QuotesUseCase {
-    override suspend fun getRandomQuotes(): Flow<ApiResponse<List<Quote>>> = quotesRepository.getRandomQuotes()
+    override suspend fun getRandomQuotes() = quotesRepository.getRandomQuotes()
     override suspend fun getQuotesByAnime(anime: String) = quotesRepository.getQuotesByAnime(anime)
     override suspend fun getQuotesByCharacter(character: String) = quotesRepository.getQuotesByCharacter(character)
     override fun getFavoriteQuotes() = quotesRepository.getFavoriteQuotes()
