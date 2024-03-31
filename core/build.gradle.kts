7b5eef7aa509eb391851299f834cd17161c3e849
Nanda Arya Putra <nandaarya404@gmail.com>
2024-03-31 15:14:36 +0700

    add sql cipher for encrypt database

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id ("kotlin-parcelize")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -33,56 +33,61 @@ android {
                 "proguard-rules.pro"
             )
         }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+        buildFeatures {
+            buildConfig = true
+            viewBinding = true
+        }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+    dependencies {
+
+        implementation("androidx.core:core-ktx:1.12.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.media3:media3-common:1.3.0")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+        // Navigation
+        val navVersion = "2.7.7"
+
+        api("androidx.navigation:navigation-fragment-ktx:$navVersion")
+        api("androidx.navigation:navigation-ui-ktx:$navVersion")
+        api("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+
+        // koin
+        api("io.insert-koin:koin-core:3.3.2")
+        api("io.insert-koin:koin-android:3.3.2")
+
+        api("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+        // Room
+        val roomVersion = "2.6.1"
+        implementation("androidx.room:room-runtime:$roomVersion")
+        implementation("androidx.room:room-ktx:$roomVersion")
+        ksp("androidx.room:room-compiler:$roomVersion")
+
+        // leak canary
+        debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
+
+        // sql cipher
+        implementation ("net.zetetic:android-database-sqlcipher:4.4.0")
+        implementation ("androidx.sqlite:sqlite-ktx:2.4.0")
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.media3:media3-common:1.3.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
-    // Navigation
-    val navVersion = "2.7.7"
-
-    api("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    api("androidx.navigation:navigation-ui-ktx:$navVersion")
-    api("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
-
-    // koin
-    api ("io.insert-koin:koin-core:3.3.2")
-    api ("io.insert-koin:koin-android:3.3.2")
-
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-
-    // Room
-    val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-
-    debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.10")
 }
--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -2,16 +2,21 @@ package com.example.core.di
 
 import androidx.room.Room
 import com.example.core.data.local.room.QuoteDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
     factory { get<QuoteDatabase>().quoteDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("nanda".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             QuoteDatabase::class.java, "Quotes.db"
         ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
