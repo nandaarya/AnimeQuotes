60ec693b1def7a581684aae4480445f5c6fb6b54
Nanda Arya Putra <nandaarya404@gmail.com>
2024-03-31 16:13:44 +0700

    add certificate pinning for network

--- core/src/main/java/com/example/core/di/NetworkModule.kt
+++ core/src/main/java/com/example/core/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.example.core.di
 
 import com.example.core.BuildConfig
 import com.example.core.data.remote.network.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -11,15 +12,21 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
-        val loggingInterceptor = if(BuildConfig.DEBUG) {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         } else {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
+        val hostname = BuildConfig.BASE_URL.removePrefix("https://")
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/6NzghlvxZodPNVSMpF2JbWJV2xXW5eiOAkkkQoHGfmw=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
